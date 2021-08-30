b47fcd1a6a1f921093afcfb136a03709cdccdb3e
Muhammad Zabidi <mamduh.zabidi@gmail.com>
2021-08-30 23:54:14 +0800

    updated price grabbing script

--- ASNB_prices/grab_master.sh
+++ ASNB_prices/grab_master.sh
@@ -15,81 +15,53 @@ page=$(mktemp)
 #update 17/09/2020
 curl https://www.asnb.com.my/dpv2_thedisplay-print.php > $page
 #there has been several iterations of the access points...
+#update 30/08/2021
+curl https://www.asnb.com.my/dpv2_thedisplay-printnewBM.php > $page
 
 
-
-
-
-#grab date
-#here, I choose the Malay format
-# date_malay=$(awk '$0~"lblDate"{
-#   split($0,x,">")
-#   split(x[2],y,"<")
-#   print y[1]
-# }' $page | sed 's/ /_/g')
-
+# grab date in Malay
 date_malay=$(awk '/HARGA HARI INI/{print $4"_"$5"_"substr($6,1,4)}' $page | \
 tr [:lower:] [:upper:])
 
 echo $date_malay
 
 # grab prices and print to separate files
-price_prefix=$(mktemp)
-cat $page | awk -v pre=$price_prefix \
-'$0~/<td>AS/{
-  fund = substr($1,5)$2$3$4
-  price[fund]=1}
-$0~/<td width=/{
-  getline
-  price[fund] = $1
-  target = pre"_"fund
-  print price[fund] > target
-  print price[fund], target
-  }'
-
-
-
-#from the saved webpage, grab NAV price list
-price_list=$(mktemp)
-cat $page | \
-  awk -v OFS="\t" '$0~"GridView1"{
-    ok=1
-  }
-  (ok){
-
-    #if($0~"width:90px")    previous condition for old webpage
-     if($0~">AS")           #use the fund prefix
-    {
-      split($0,x,">")
-      split(x[2],y,"<")
-      fund_name=y[1]
-      gsub(" ","_",fund_name)
-
-      split(x[4],z,"<")
-      fund_price=z[1]
-
-      print fund_name,fund_price
-
-    }
-  }' > $price_list
+prefix=$(mktemp)
+cat $page | awk -v pre=$prefix \
+  '$0~/<td>AS/{
+    fund = substr($1,5)$2$3$4
+    price[fund]=1}
+  $0~/<td width=/{
+    getline
+    price[fund] = $1
+    target = pre"_"fund
+    print price[fund] > target
+    print fund, price[fund]
+    }'
 
 #check if date is already present
 #if not, append new NAB price
 if grep "$date_malay" daily_prices.txt; then
     echo "Date already present"
   else
-    cat $price_list | \
-      awk -v OFS="\t" -v p="$date_malay" '{name[NR]=$2}
-        END{
-          line=p
-          for(i=1;i<=NR;i++)
-            line=line"\t"name[i]
-          print line
-        }' >> daily_prices.txt          #here, you can set daily_prices.txt to your own output
+    echo -en $date_malay"\t"$(cat ${prefix}_ASN)"\t"\
+    $(cat ${prefix}_ASNEquity2)"\t"\
+    $(cat ${prefix}_ASNImbang1)"\t"\
+    $(cat ${prefix}_ASNEquity3)"\t"\
+    $(cat ${prefix}_ASNSara1)"\t"\
+    $(cat ${prefix}_ASB)"\t"\
+    $(cat ${prefix}_ASM2Wawasan)"\t"\
+    $(cat ${prefix}_ASM)"\t"\
+    $(cat ${prefix}_ASB3Didik)"\t"\
+    $(cat ${prefix}_ASM3)"\t"\
+    $(cat ${prefix}_ASB2)"\t"\
+    $(cat ${prefix}_ASNSara2)"\t"\
+    $(cat ${prefix}_ASNEquity5)"\t"\
+    $(cat ${prefix}_ASNImbang3Global)"\n" >> daily_prices.txt
 fi
 
 #clean up
-rm $page $price_list
+rm $page $prefix*
 
 exit 0
 
